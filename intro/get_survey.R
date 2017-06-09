474ca806ebeed79fcf6c650574e54aa362c0884f
Joe Brew <joebrew@gmail.com>
2017-06-09 14:29:53 +0200

    a lot of mozambique related class files - shouldnt break anything

--- intro/get_survey.R
+++ intro/get_survey.R
@@ -70,3 +70,111 @@ for (i in 1:nrow(participants)){
   # file.remove(report_name)
   # Sys.sleep(1)
 }
+
+
+# University of Toronto course
+
+the_url <-
+  "https://docs.google.com/spreadsheets/d/16U9wC07Ub8a3Q6JIymQLjKT-tSTJ0-0rUX_1H3DBlAg/edit?usp=sharing"
+
+toronto <- gsheet2tbl(the_url)
+toronto <- data.frame(toronto)
+
+names(toronto) <- tolower(names(toronto))
+
+# Write to each participant
+from = 'joe@databrew.cc'
+password = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+
+for (i in 20:nrow(toronto)){
+  destinataire <- c(toronto$email[i], "benmbrew@gmail.com")
+  body <- paste0('Dear ',
+                 toronto$name[i],
+                 ',\n\n',
+                 "I'm writing because I thought that perhaps some of your students ",
+                 "might be interested in taking this R course being offered in Toronto this weekend:\n\n",
+                 "http://www.databrew.cc/Rworkshops.html#openModal1\n\n",
+                 '\n',
+                 'It should provide a solid foundation for those just getting into statistical computing - and there is a discount for university students.\n\n',
+                 'Best,\n',
+                 'Joe Brew\n\n')
+  
+  send.mail(from = from,
+            # attach.files = report_name,
+            to = destinataire,
+            subject = 'R course in Toronto this weekend',
+            body = body,
+            smtp = list(host.name = "smtp.gmail.com", 
+                        port = 465, 
+                        user.name = from,
+                        passwd=password, 
+                        ssl=TRUE),
+            authenticate = TRUE,
+            send = TRUE)
+  # file.remove(report_name)
+  Sys.sleep(1)
+}
+
+# RYERSON
+the_url <- "https://docs.google.com/spreadsheets/d/1vaXJuAqHf8S9sFJl3ZmYnIBCgsc_GKVthMja9nPWCzA/edit?ts=592ece3c#gid=0"
+
+
+toronto <- gsheet2tbl(the_url)
+toronto <- data.frame(toronto)
+names(toronto) <- tolower(names(toronto))
+
+# # Clean up
+# toronto$email <- 
+#   gsub('(at)', '@', toronto$email, fixed = TRUE)
+# toronto$email <- paste0(toronto$email,
+#                         ifelse(is.na(toronto$email2), '',
+#                                toronto$email2))
+get_name <- function(x){
+  x_split <- strsplit(x, ' ')
+  x_split <- unlist(x_split)
+  out <- x_split[2]
+  if(length(x_split) == 2){
+    out <- paste0('Dr. ', out)
+  }
+  return(out)
+}
+for (i in 1:nrow(toronto)){
+  toronto$name[i] <-
+    get_name(toronto$name[i])
+}
+
+
+# Write to each participant
+from = 'joebrew@gmail.com'
+password = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+
+for (i in 1:nrow(toronto)){
+  destinataire <- c(toronto$email[i], "info@databrew.cc")
+  body <- paste0('Dear ',
+                 toronto$name[i],
+                 ',\n\n',
+                 "I'm writing because I thought that perhaps some of your students ",
+                 "might be interested in taking this R course being offered in Toronto this weekend:\n\n",
+                 "http://www.databrew.cc/Rworkshops.html#openModal1\n\n",
+                 '\n',
+                 'It should provide a solid foundation for those just getting into statistical computing - and there is a discount for university students.\n\n',
+                 'Best,\n',
+                 'Joe Brew\n\n')
+  
+  send.mail(from = from,
+            # attach.files = report_name,
+            to = destinataire,
+            subject = 'R course in Toronto this weekend',
+            body = body,
+            smtp = list(host.name = "smtp.gmail.com", 
+                        port = 465, 
+                        user.name = from,
+                        passwd=password, 
+                        ssl=TRUE),
+            authenticate = TRUE,
+            send = TRUE)
+  # file.remove(report_name)
+  Sys.sleep(1)
+}
